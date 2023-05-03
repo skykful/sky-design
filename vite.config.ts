a7846c5e98cc51a19063e76827a56540c7da4a72
skykful <12739501+skykful@user.noreply.gitee.com>
2023-05-03 18:39:26 +0800

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ViteCompress from 'vite-plugin-compression';
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     AutoImport({
@@ -16,11 +19,41 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    ViteCompress({
+      ext: '.gz'
+    }),
+    visualizer({
+      emitFile: true,
+      filename: "dist/stats.html",
+      open:true 
     })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    minify: "terser",
+    terserOptions: { 
+      compress: {
+        drop_console: true, //剔除console,和debugger
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        manualChunks: {
+          vue: ['vue', 'vue-router', 'pinia'],
+          axios: ['axios'],
+          echarts: ['echarts'],
+          'element-plus': ['element-plus', '@element-plus/icons-vue']
+        }
+      }
     }
   }
 })
